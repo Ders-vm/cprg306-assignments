948a1ae290132f839c9b4fbda6514b4d6d7dd366
Anders von Mirbach <121372433+Ders-vm@users.noreply.github.com>
2024-01-17 15:12:00 -0700

    fixed bugs

---
+++ app/week-3/page.js
@@ -0,0 +1,8 @@
+
+export default function Page() {
+    return (
+      <main>
+        <h1> CPRG 306: Web Development 2 </h1>
+      </main>
+    );
+  }
---
+++ app/week-4/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+      <main>
+        <h1> CPRG 306: Web Development 2 </h1>
+      </main>
+    );
+  }
