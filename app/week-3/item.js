735e69fea9e77fc7845c66c64b0b0aae7610c4d7
Anders von Mirbach <121372433+Ders-vm@users.noreply.github.com>
2024-01-26 15:23:11 -0700

    Added some color and cleared up some clutter

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Item({ name, quantity, category }) {
     return (
-        <section className="border-2 p-2 m-2 background-red">
+        <section style={{ border: '2px solid teal' }} className="p-2 m-2">
             <h1 className="text-2xl font-bold">{name}</h1>
             <p>Category: {category}</p>
             <p>Quantity: {quantity}</p>
