bfe7da3ef6e5968c14f94ade0be767e79dbcb09c
Anders von Mirbach <121372433+Ders-vm@users.noreply.github.com>
2024-01-28 20:38:20 -0700

    finally got the home button working

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,13 +1,13 @@
 import ItemList from "./item-list";
 import Link from 'next/link';
+import HomeButton from "../Functions/home";
 
 export default function Page() {
     return (
       <main>
         <h1> CPRG 306: Web Development 2 </h1>
-        <Link href="/"> Home </Link>
         <h2> Week 3 </h2>
-        
+        <HomeButton/>
         <ItemList/>
       </main>
     );
