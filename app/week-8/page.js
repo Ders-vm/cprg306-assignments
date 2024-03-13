84f26cc5ed2bea43eb04afc61be10bbfd078340f
Anders von Mirbach <121372433+Ders-vm@users.noreply.github.com>
2024-03-13 10:36:03 -0600

    trying to compile

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,10 +1,8 @@
 // Code for the page component that uses the useUserAuth hook to access the user object and the gitHubSignIn and firebaseSignOut functions. 
 
 "use client";
-import Link from "next/link";
 import { useEffect } from "react";
 import { useUserAuth } from "./_utils/auth-context";
-import {HomeButton} from "../Components/home";
 
 function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
